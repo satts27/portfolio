71166641016d2f6d3c80f8d6424fd3a2bcda4237
satts27 <suryasatishganiga@gmail.com>
2024-06-19 17:42:39 +0530

    initial website

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        heading: ["Teko", "sans-serif"],
+        heading2: ["Anton", "sans-serif"],
+        heading3: ["Kalam", "cursive"],
+      },
+      colors: {
+        creme: "#ede6dc",
+        occur: "#6d5444",
+        pinkish: "#c6b19d",
+        ocshadow: "#896e57",
+      },
+      scale: {
+        200: "2.00",
+      },
     },
   },
   plugins: [],
